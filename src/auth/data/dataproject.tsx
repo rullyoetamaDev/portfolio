c05a4da3bffa711ca2ed84a49e129e8f08c72cba
rullyoetamaDev <rullyhpfq2035au@gmail.com>
2025-07-20 22:12:29 +0700

    first commit portfolio

--- src/auth/data/dataproject.tsx
+++ src/auth/data/dataproject.tsx
@@ -10,28 +10,8 @@ export const HERO_CONTENT = `I am a Frontend Developer with more than 3 years of
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 3 years of middle experience, I have worked with a variety of technologies, including React, Next.js, Sequelize, Express, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
-  // {
-  //   year: "2023 - Present",
-  //   role: "Senior Full Stack Developer",
-  //   company: "Google Inc.",
-  //   description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-  //   technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  // },
-  // {
-  //   year: "2022 - 2023",
-  //   role: "Frontend Developer",
-  //   company: "Adobe",
-  //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-  //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  // },
-  // {
-  //   year: "2021 - 2022",
-  //   role: "Full Stack Developer",
-  //   company: "Facebook",
-  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  // },
   {
+    id : 1,
     year: "Aug 2021 - Mar 2022",
     role: "Full Stack Developer",
     company: "PT Whiteopen Teknologi",
@@ -39,6 +19,37 @@ export const EXPERIENCES = [
     technologies: ["ASP.NET MVC", "jQuery", "JavaScript", "PostgreSQL","Spring Boot","Microsoft SQL Server","Java","Software Development Life Cycle (SDLC)","Application Programming Interfaces (API)"],
   },
 ];
+
+// export const EXPERIENCES = [
+//   // {
+//   //   year: "2023 - Present",
+//   //   role: "Senior Full Stack Developer",
+//   //   company: "Google Inc.",
+//   //   description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+//   //   technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+//   // },
+//   // {
+//   //   year: "2022 - 2023",
+//   //   role: "Frontend Developer",
+//   //   company: "Adobe",
+//   //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+//   //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+//   // },
+//   // {
+//   //   year: "2021 - 2022",
+//   //   role: "Full Stack Developer",
+//   //   company: "Facebook",
+//   //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+//   //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+//   // },
+//   {
+//     year: "Aug 2021 - Mar 2022",
+//     role: "Full Stack Developer",
+//     company: "PT Whiteopen Teknologi",
+//     description: `I am work as Programmer in one of vendor on Jakarta, while working i use programming languages like C# with ASP .NET framework, Javascript, Jquery, HTML, CSS, Ajax, Power BI. And several office application like Microsoft Excel and Microsoft Word. Sometime i have meeting with my clients, to discuss additional or bugs by user, after that i do system analysis and work on it.`,
+//     technologies: ["ASP.NET MVC", "jQuery", "JavaScript", "PostgreSQL","Spring Boot","Microsoft SQL Server","Java","Software Development Life Cycle (SDLC)","Application Programming Interfaces (API)"],
+//   },
+// ];
 
 export const PROJECTS = [
   {
